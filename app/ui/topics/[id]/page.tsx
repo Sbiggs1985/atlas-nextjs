c6dc23e47f75cc981a0c30ca076490bebe26f614
Stephaine Biggs <78326908+Sbiggs1985@users.noreply.github.com>
2025-02-04 18:55:33 -0600

    update

--- app/ui/topics/[id]/page.tsx
+++ app/ui/topics/[id]/page.tsx
@@ -1,14 +1,14 @@
-interface TopicProps {
-  params: { id: string }; // Get the dynamic ID from the URL
+interface TopicPageProps {
+  params: { id: string };
 }
 
-export default function TopicPage({ params }: { params: { id: string } }) {
+export default function Page({ params }: TopicPageProps) {
+  console.log("Params:", params);
+
   return (
-    <main>
+    <div>
       <h1>Topic: {decodeURIComponent(params.id)}</h1>
       <p>Here are all the questions related to {decodeURIComponent(params.id)}.</p>
-    </main>
+    </div>
   );
 }
-
-  
